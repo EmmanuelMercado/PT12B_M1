3023622d597ef8ffad132c99dab53be577564d2e
Emmanuel Mercado <Emmanuel.Mercado@flex.com>
2023-02-22 19:17:31 -0600

    Homework 1 listo

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,8 +1,38 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+   var suma = 0;
+   var exponente= num.length-1;
+   for (var i=0; i<num.length; i++){
+      suma = suma + (num[i])*Math.pow(2,exponente);
+      exponente--;
+   }
+   return (suma);
+   }
+function DecimalABinario(num) {
+   var binario = "";
+      var esDivisible = true;
+   while (esDivisible===true){
+      if (num%2==1){
+         binario = "1"+ binario; //100
+      }
+      else{
+         binario = "0"+ binario; //100
+      }
+      
+      
+      if (num>=2){
+         esDivisible=true;
+      }
+      else{
+         esDivisible=false;
+      }
+      num = parseInt(num/2);
+   }
+   return binario;
+}
 
-function DecimalABinario(num) {}
+
 
 module.exports = {
    BinarioADecimal,
